9ffd4ec5d832839369dc1520a78e409090677ad6
pheno-cat <catherinejalomo@gmail.com>
2020-10-17 18:45:10 -0500

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "1234567890";
-var specialChar = "!@#$%^&*()_+=|}{[]:',.?/<>"
+
+var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "1234567890";
+var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -19,27 +21,77 @@ function writePassword() {
 
 
 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  var 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* <div class="card-body">
-<textarea
-  readonly
-  id="password"
-  placeholder="Your Secure Password"
-  aria-label="Generated Password"
-></textarea>
-</div> */
+  // let passwordTextarea = document.querySelector("#password");
+  // let generateButton = document.querySelector("#generate");
+  // let copyButton = document.querySelector("#copy");
+  
+  // const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  // const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+  // const numericalCharacters = "0123456789";
+  // const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  
+  // generateButton.addEventListener("click", generatePassword);
+  // copyButton.addEventListener("click", copyPassword);
+  
+  // function generatePassword() {
+  
+  //     let newPassword = "";
+  //     let passwordCharset = "";
+  
+  //     let passwordLengthInput = prompt("How many characters would you like your password to be?");
+  
+  //     if (Number(passwordLengthInput < 8) | Number(passwordLengthInput > 128) | isNaN(passwordLengthInput)) {
+  //         alert("Please enter a number between 8 and 128!");
+  //         return;
+  //     } 
+  
+  //     let lowercaseCharactersInput = confirm("Would you like lowercase letters to be included in your password?");
+  
+  //     if (lowercaseCharactersInput) {
+  //         passwordCharset += lowercaseCharacters;
+  //     }
+  
+  //     let uppercaseCharactersInput = confirm("Would you like uppercase letters to be included in your password?");
+  
+  //     if (uppercaseCharactersInput) {
+  //         passwordCharset += uppercaseCharacters;
+  //     }
+  
+  //     let numericalCharactersInput = confirm("Would you like numbers to be included in your password?");
+  
+  //     if (numericalCharactersInput) {
+  //         passwordCharset += numericalCharacters;
+  //     }
+  
+  //     let specialCharactersInput = confirm("Would you like special characters to be included in your password?");
+  
+  //     if (specialCharactersInput) {
+  //         passwordCharset += specialCharacters;
+  //     }
+  
+  //     if (passwordCharset === "") {
+  //         alert("Please select at least one type of character you would like to include!");
+  //     }
+      
+  //     if (Number(passwordLengthInput >= 8) && Number(passwordLengthInput <= 128)) {
+  //         for (var i = 0; i < passwordLengthInput; i++) {
+  //             newPassword += passwordCharset.charAt(Math.floor(Math.random() * passwordCharset.length));
+  //         };
+  //         localStorage.setItem("password", newPassword);
+  //         let newPasswordStr = localStorage.getItem("password");
+  //         passwordTextarea.textContent = newPasswordStr;
+  //     };
+  // };
+  
+  // function copyPassword() {
+  //     passwordTextarea.select();
+  //     document.execCommand("copy");
+  //     passwordTextarea.innerHTML = "";
+  // };
